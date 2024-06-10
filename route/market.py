100446a04264c027e55bc2ec05679be7b1366081
Yeying-Xingchen <yeyingxingchen@yeah.net>
2024-06-10 20:05:00 +0800

    新增：主题页面。

--- route/market.py
+++ route/market.py
@@ -8,10 +8,19 @@ app = Blueprint('market', __name__)
 appearance = config.get_config("appearance")
 
 registry = "https://raw.githubusercontent.com/seabook-panel/market/main/registry/"
+
 @app.route('/')
-def index():
+def apps():
     if auth() == False:
         return render_template('login.html')
-    response = requests.get(registry+"plugins.json")
+    response = requests.get(registry+"apps.json")
     plugin_list = json.loads(response.text)
-    return render_template('market/plugins.html', plugin_list = plugin_list,appearance=appearance)
+    return render_template('market/apps.html', plugin_list = plugin_list,appearance=appearance)
+
+@app.route('/theme')
+def theme():
+    if auth() == False:
+        return render_template('login.html')
+    response = requests.get(registry+"theme.json")
+    theme_list = json.loads(response.text)
+    return render_template('market/theme.html', theme_list = theme_list,appearance=appearance)
