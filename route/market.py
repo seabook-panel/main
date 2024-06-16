e0f2e01786dfde9b10f058d536ae508c5c7ef52a
Yeying-Xingchen <yeyingxingchen@yeah.net>
2024-06-16 23:21:18 +0800

    新增 安装返回。

--- route/market.py
+++ route/market.py
@@ -1,10 +1,14 @@
 import os
+import select
+import sys
 from flask import Blueprint, render_template, redirect
 import config
 import requests
 import json
 import zipfile
 import shutil
+import subprocess
+
 
 from auth import auth
 app = Blueprint('market', __name__)
@@ -74,5 +78,13 @@ def install_apps(url):
     temp_path = install_path +"temp/apps.py"
     with open(temp_path, "w", encoding='utf-8') as f:
         f.write(response.text)
-    os.system("python "+temp_path+" "+install_path)
-    return "安装成功！"
+    cmd = "python "+temp_path+" "+install_path
+    content = "安装返回：<br>"
+    p = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    for line in iter(p.stdout.readline, b''):
+        try:
+            content = content+str(line.decode('utf-8'))+"<br>"
+        except UnicodeDecodeError:
+            content = content+str(line.decode('gbk'))+"<br>"
+    p.wait()
+    return content
