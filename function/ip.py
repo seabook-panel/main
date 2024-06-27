52d25dbcfcaa3a26290a40dba218cc2b35dc6123
Yeying-Xingchen <yeyingxingchen@yeah.net>
2024-06-27 22:29:01 +0800

    修复 【主页】部分机器无法获取IP。

--- function/ip.py
+++ function/ip.py
@@ -4,7 +4,10 @@ from json import load
 
 def local_ip():
     hostname = socket.getfqdn(socket.gethostname())
-    addr = socket.gethostbyname(hostname)
+    try:
+        addr = socket.gethostbyname(hostname)
+    except socket.gaierror:
+        addr = hostname
     return addr
 
 
@@ -12,6 +15,6 @@ def external_ip():
     try:
         my_ip = load(urlopen('http://httpbin.org/ip'))['origin']
         return my_ip
-    except:
+    except Exception:
         return "获取公网IP失败！"
         # 防止由于测试给API带来太大压力，这里返回错误即可。
